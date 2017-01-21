47a4b81ccacbb5799dc90bc9c04a8c97c4f1bb70
Ian Webster <ianw_github@ianww.com>
2017-01-21 16:30:49 +0300

    spaces

--- public/main.js
+++ public/main.js
@@ -2,9 +2,9 @@ $(function() {
   var handler = StripeCheckout.configure({
     key: 'pk_test_DfWFqx41f7WRUXrMKEXIOYXQ',
     token: function(token) {
-      document.body.style.cursor='wait';
+      document.body.style.cursor = 'wait';
       $.get('/pay?email=' + token.email + '&tok=' + token.id + '&amount=500', function(resp) {
-        document.body.style.cursor='default';
+        document.body.style.cursor = 'default';
         if (resp === 'ok') {
           alert('Thanks!');
         } else {
